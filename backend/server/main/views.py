7e8addaab8034257d25443692c8d67d484acbb2b
Bagurgl <danek_penek@mail.ru>
2022-04-13 17:43:48 +0300

    ...
    тут надо будет проверить обновление check_point

--- backend/server/main/views.py
+++ backend/server/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from testing_pyshark import check_point
+import testing_pyshark
+
 
 def index(request):
     return render(request, 'main/index.html')
@@ -9,9 +10,11 @@ def index(request):
 def testdb(request):
     return HttpResponse('dffsgwrg')
 
+
 def min(request):
+    check_point = 0
+    testing_pyshark.scan(check_point)
     if check_point == 1:
         return HttpResponse('Майнинг')
     else:
         return HttpResponse('Всё тихо')
-
--- backend/server/testing_pyshark.py
+++ backend/server/testing_pyshark.py
@@ -1,64 +1,67 @@
 import pyshark
 import time
 import re
-networkInterface = "4"
-
-# define capture object
-capture = pyshark.LiveCapture(interface=networkInterface, bpf_filter='tcp')
-
-print("listening on %s" % networkInterface)
-
-for packet in capture.sniff_continuously(packet_count=10):
-    # adjusted output
-    try:
-        # get timestamp
-        localtime = time.asctime(time.localtime(time.time()))
-
-        # get packet content
-        protocol = packet.transport_layer  # protocol type
-        src_addr = packet.ip.src  # source address
-        src_port = packet[protocol].srcport  # source port
-        dst_addr = packet.ip.dst  # destination address
-        dst_port = packet[protocol].dstport  # destination port
-        pkt_info = packet.tcp.payload
-        # hex to utf
-
-        hex_split = pkt_info.replace(':', '')
-
-        mining = '6d696e696e67'
-        check_point = 0
-        if re.search(mining, hex_split, flags=0):
-            print('УРА')
-            check_point = 1
-
-        # output packet info
-        if len(pkt_info) > 5:
-            print("%s IP %s:%s <-> %s:%s (%s)     %s" % (
-                localtime, src_addr, src_port, dst_addr, dst_port, protocol, hex_split))
-
-        ##pkt_info = bytes.fromhex(pkt_info)
-        ##print(pkt_info.decode('utf-8'))
-        """
-        dir(packet)  # for entire packet
-        dir(packet.ip)  # for ip layer
-        dir(packet.tcp)  # for tcp layer
-        print(packet.tcp)  # one way
-        packet.tcp.pretty_print()
-        """
-
-    except AttributeError as e:
-        # ignore packets other than TCP, UDP and IPv4
-        pass
-"""
-
-cap = pyshark.LiveCapture(interface='4', bpf_filter='tcp')
-
-cap.sniff(packet_count=10)
 
 
-def print_dns_info(pkt):
-    print('DNS Request from %s: %s' % (pkt.ip.src, pkt.tcp.payload))
 
+def scan(check_point):
+    networkInterface = "4"
 
-cap.apply_on_packets(print_dns_info, timeout=100)
-"""
+    # define capture object
+    print("listening on %s" % networkInterface)
+    capture = pyshark.LiveCapture(interface=networkInterface, bpf_filter='tcp')
+    for packet in capture.sniff_continuously(packet_count=50):
+        # adjusted output
+        try:
+            # get timestamp
+            localtime = time.asctime(time.localtime(time.time()))
+
+            # get packet content
+            protocol = packet.transport_layer  # protocol type
+            src_addr = packet.ip.src  # source address
+            src_port = packet[protocol].srcport  # source port
+            dst_addr = packet.ip.dst  # destination address
+            dst_port = packet[protocol].dstport  # destination port
+            pkt_info = packet.tcp.payload
+            # hex to utf
+
+            hex_split = pkt_info.replace(':', '')
+
+            mining = '6d696e696e67'
+            check_point = 0
+            if re.search(mining, hex_split, flags=0):
+                print('УРА')
+                check_point = 1
+
+            # output packet info
+            if len(pkt_info) > 5:
+                print("%s IP %s:%s <-> %s:%s (%s)     %s" % (
+                    localtime, src_addr, src_port, dst_addr, dst_port, protocol, hex_split))
+
+            ##pkt_info = bytes.fromhex(pkt_info)
+            ##print(pkt_info.decode('utf-8'))
+            """
+            dir(packet)  # for entire packet
+            dir(packet.ip)  # for ip layer
+            dir(packet.tcp)  # for tcp layer
+            print(packet.tcp)  # one way
+            packet.tcp.pretty_print()
+            """
+
+        except AttributeError as e:
+            # ignore packets other than TCP, UDP and IPv4
+            pass
+    return check_point
+    """
+    
+    cap = pyshark.LiveCapture(interface='4', bpf_filter='tcp')
+    
+    cap.sniff(packet_count=10)
+    
+    
+    def print_dns_info(pkt):
+        print('DNS Request from %s: %s' % (pkt.ip.src, pkt.tcp.payload))
+    
+    
+    cap.apply_on_packets(print_dns_info, timeout=100)
+    """
