e9e319d4a09628014123ab2197af8641b3366e45
Bagurgl <danek_penek@mail.ru>
2022-04-13 13:58:46 +0300

    working with HEX

--- backend/server/main/testing_pyshark.py
+++ backend/server/main/testing_pyshark.py
@@ -1,7 +1,6 @@
 import pyshark
 import time
 
-
 networkInterface = "4"
 
 # define capture object
@@ -9,7 +8,7 @@ capture = pyshark.LiveCapture(interface=networkInterface, bpf_filter='tcp')
 
 print("listening on %s" % networkInterface)
 
-for packet in capture.sniff_continuously(packet_count=300):
+for packet in capture.sniff_continuously(packet_count=1000):
     # adjusted output
     try:
         # get timestamp
@@ -21,14 +20,33 @@ for packet in capture.sniff_continuously(packet_count=300):
         src_port = packet[protocol].srcport  # source port
         dst_addr = packet.ip.dst  # destination address
         dst_port = packet[protocol].dstport  # destination port
-        pkt_info = packet.tcp.payload
+        pkt_info = packet.tcp.segment_data
+        # hex to utf
+
+        #hex_split = pkt_info.split(':')
+        #hex_as_chars = map(lambda hex: chr(int(hex, 16)), hex_split)
+        #res = ''.join(hex_as_chars)
+        #b = bytes.fromhex(res).decode('utf-8-be')
+        
+        #res = res.split('')
         # output packet info
-        print("%s IP %s:%s <-> %s:%s (%s)     %s" % (localtime, src_addr, src_port, dst_addr, dst_port, protocol, pkt_info))
+        if len(pkt_info) > 5:
+            print("%s IP %s:%s <-> %s:%s (%s)     %s" % (
+                localtime, src_addr, src_port, dst_addr, dst_port, protocol, pkt_info))
+
+        ##pkt_info = bytes.fromhex(pkt_info)
+        ##print(pkt_info.decode('utf-8'))
+        """
+        dir(packet)  # for entire packet
+        dir(packet.ip)  # for ip layer
+        dir(packet.tcp)  # for tcp layer
+        print(packet.tcp)  # one way
+        packet.tcp.pretty_print()
+        """
 
     except AttributeError as e:
         # ignore packets other than TCP, UDP and IPv4
         pass
-    print(" ")
 """
 
 cap = pyshark.LiveCapture(interface='4', bpf_filter='tcp')
