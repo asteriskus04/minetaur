d5bc168fa769b44116c9198849d2a731aaa8a7f3
YTDantes <dan12332123@gmail.com>
2022-04-16 14:19:14 +0300

    updatre json

--- backend/server/testing_pyshark.py
+++ backend/server/testing_pyshark.py
@@ -2,9 +2,20 @@ import pyshark
 import json
 import re
 
-data_json={}
+data_json = {}
+server = {
 
-def gen_data(ip_points, check_point):
+        "id": "Server",
+        "height": 70,
+        "fill": {
+            "src": "static/main/img/server_icon.png"
+        }
+    }
+
+
+
+
+def gen_data(ip_points, check_point, pcnum):
     id = ''
     status = ''
     ip = ''
@@ -12,8 +23,9 @@ def gen_data(ip_points, check_point):
     height = 0
     fill = ''
 
+
     pc = {
-        "id": "PC_1_1",
+        "id": "PC_1_" + str(pcnum),
         "status": check_point,
         "ip": ip_points,
         "mac": "28:E9:46:6C:58:EF",
@@ -23,6 +35,7 @@ def gen_data(ip_points, check_point):
         }
     }
     return pc
+
 
 def write_json(pc_data):
     try:
@@ -35,14 +48,28 @@ def write_json(pc_data):
     with open('persons.json', 'w') as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
 
+def write_nodes():
+    with open('data.json', 'w') as outfile:
+        json.dump([], outfile)
+    nodes = {}
+    edges = {}
+    try:
+        data = json.load(open('persons.json'))
+    except:
+        data = []
 
+    itog = {
+        'nodes': data,
+        'edges': data
+    }
+    with open('data.json', 'w') as file:
+        json.dump(itog, file, indent=4, ensure_ascii=False)
 
-
-
-
+write_json(server)
 def scan():
     global check_point
     networkInterface = "5"
+    pcnum = 1
     ip_points = ''
     ip_points_1 = ''
     check_point = 0
@@ -63,21 +90,18 @@ def scan():
             mining = '6d'
 
             if re.search(mining, hex_split, flags=0):
+                pcnum += 1
                 check_point = 'Майнинг'
-                ip_points += src_addr
-                #ip_points_1 += dst_addr
-
-
+                write_json(gen_data(src_addr, check_point, pcnum))
+                # ip_points_1 += dst_addr
 
             # output packet info
             if len(pkt_info) > 5:
-                print("IP %s <-> %s (%s)" % (
-                    src_addr, dst_addr, hex_split))
+                print("IP %s <-> %s" % (
+                    src_addr, dst_addr))
 
         except AttributeError as e:
             # ignore packets other than TCP, UDP and IPv4
             pass
-        write_json(gen_data(ip_points,check_point))
+        write_nodes()
     return check_point, ip_points
-
-
