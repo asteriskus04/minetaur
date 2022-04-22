0bc10bf4374a80349ffa067bae872099850c0b79
Bagurgl <danek_penek@mail.ru>
2022-04-22 19:03:14 +0300

    fix gitignore

--- backend/server/main/static/main/js/script.js
+++ backend/server/main/static/main/js/script.js
@@ -1,90 +1,86 @@
-$(function () {
-    $("#load").delay(2999).slideUp(300);
-});
-
 $('document').ready(function () {
     $('.help').hide();
     $('.about').hide();
+    $('#network-table').hide();
 })
 
 function hide() {
     $('.help').fadeOut();
 }
 function show() {
-    $('.help').fadeIn(300);
+    $('.help').fadeIn();
 }
 
 function hide_about() {
     $('.about').fadeOut();
 }
 function show_about() {
-    $('.about').fadeIn(300);
-}
-
-function exp() {
-    alert('Эта функция скоро появится!');
+    $('.about').fadeIn();
 }
 
 var keys = [];
+function getmap() {
+    fetch('http://localhost:8000/api/getmap')
+        .then(d => d.json())
+        .then(json => {
+            //Карта сети
+            anychart.data.loadJson(json, function (data) {
+                // создание графа из загруженных данных
+                var chart = anychart.graph(data);
 
-//Карта сети
-anychart.data.loadJsonFile('static/main/js/data1.json', function (data) {
-    // создание графа из загруженных данных
-    var chart = anychart.graph(data);
+                // рисование графа
+                chart.container("network-map").draw();
 
-    // рисование графа
-    chart.container("network-map").draw();
+                // название графа вверху схемы
+                chart.title(false);
 
-    // название графа вверху схемы
-    chart.title(false);
+                // подписи у точек
+                chart.nodes().labels().enabled(false);
+                chart.nodes().labels().format("{%id}");
+                chart.nodes().labels().fontSize(12);
+                chart.nodes().labels().fontWeight(600);
 
-    // подписи у точек
-    chart.nodes().labels().enabled(false);
-    chart.nodes().labels().format("{%id}");
-    chart.nodes().labels().fontSize(12);
-    chart.nodes().labels().fontWeight(600);
+                // запрет на перемещение точек
+                chart.interactivity().nodes(true);
 
-    // запрет на перемещение точек
-    chart.interactivity().nodes(true);
+                // запрет на зум
+                chart.interactivity().zoomOnMouseWheel(true);
+                // запрет на скролл по вертикали
+                chart.interactivity().scrollOnMouseWheel(false);
 
-    // запрет на зум
-    chart.interactivity().zoomOnMouseWheel(true);
-    // запрет на скролл по вертикали
-    chart.interactivity().scrollOnMouseWheel(false);
+                // угол наклона графа
+                chart.rotation(30);
 
-    // угол наклона графа
-    chart.rotation(30);
+                // всплывающие окна у точек
+                chart.nodes().tooltip().useHtml(true);
+                chart.nodes().tooltip().format(
+                    "<span style='font-weight:bold; color:white;'>{%id}: {%status}</span><br><span style='color:white;'>IP-адрес: {%ip}<br>MAC-адрес: {%mac}</span>"
+                );
 
-    // всплывающие окна у точек
-    chart.nodes().tooltip().useHtml(true);
-    chart.nodes().tooltip().format(
-        "<span style='font-weight:bold; color:white;'>{%id}: {%status}</span><br><span style='color:white;'>IP-адрес: {%ip}<br>MAC-адрес: {%mac}</span>"
-    );
+                // подписи на рёбрах
+                chart.edges().tooltip(false);
 
-    // подписи на рёбрах
-    chart.edges().tooltip(false);
-
-    // настройка отображения рёбер
-    chart.edges().normal().stroke("#ffffff", 2);
-    chart.edges().hovered().stroke("#ffa000", 2, "10 5");
-    chart.edges().selected().stroke("#ffa000", 4);
+                // настройка отображения рёбер
+                chart.edges().normal().stroke("#ffffff", 2);
+                chart.edges().hovered().stroke("#ffa000", 2, "10 5");
+                chart.edges().selected().stroke("#ffa000", 4);
 
 
-    //настройка фона
-    chart.background().fill("#797ba1");
-    /*
-        chart.background().fill({
-            src: "/static/main/img/bg.svg",
-            mode: "stretch"
-        });
-    */
-})
+                //настройка фона
+                chart.background().fill("#A6B1E1");
+                /*
+                    chart.background().fill({
+                        src: "/static/main/img/bg.svg",
+                        mode: "stretch"
+                    });
+                */
+            })
+        })
+        .catch(e => alert(e))
+}
+function graph() {
 
-// Обновление карты
-function refr() {
-    $('#network-map').remove();
-    $('main').append('<div id="network-map">');
-
+    //Карта сети
     anychart.data.loadJsonFile('static/main/js/data1.json', function (data) {
         // создание графа из загруженных данных
         var chart = anychart.graph(data);
@@ -128,14 +124,39 @@ function refr() {
 
 
         //настройка фона
-        chart.background().fill("#797ba1");
+        chart.background().fill("#A6B1E1");
         /*
             chart.background().fill({
                 src: "/static/main/img/bg.svg",
                 mode: "stretch"
             });
         */
-        // fit the chart to the container
-
     })
+}
+
+function mkmp() {
+    $('main').append('<section id="load"><img src="static/main/img/load.gif" alt="Анимация загрузки" class="load_anm"><p>Загрузка карты...</p></section>');
+    $("#load").delay(5999).slideUp(300);
+    $('.hello').remove();
+    $('main').append('<button class="refr" onclick="refr();">Обновить карту</button>');
+    $('main').append('<a href="data.json" download><button class="exp" onclick="exp();">Экспорт в файл</button></a>');
+    $('main').append('<button class="change" onclick="change();">Сменить вид</button>');
+    graph();
+}
+
+function change() {
+    $('#network-map').toggle(); //включает/выключает элемент id="network-map"
+    $('#network-table').toggle(); //включает/выключает элемент id="network-table"
+    $('.refr').toggle();
+}
+
+// Обновление карты
+function refr() {
+
+    $('#network-map').remove();
+    $('#load').remove();
+    $('main').append('<section id="load"><img src="static/main/img/load.gif" alt="Анимация загрузки" class="load_anm"><p>Загрузка карты...</p></section>');
+    $("#load").delay(99).slideUp();
+    $('main').append('<div id="network-map">');
+    graph();
 }
