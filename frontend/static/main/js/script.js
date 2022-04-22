f9a08271eabadc37b0579a12147d0b2bd2ca94e2
Ivan <kotowvanya@gmail.com>
2022-04-22 18:34:38 +0300

    init froooont

--- frontend/static/main/js/script.js
+++ frontend/static/main/js/script.js
@@ -19,7 +19,67 @@ function show_about() {
 }
 
 var keys = [];
+function getmap() {
+    fetch('http://localhost:8000/api/getmap')
+        .then(d => d.json())
+        .then(json => {
+            //Карта сети
+            anychart.data.loadJson(json, function (data) {
+                // создание графа из загруженных данных
+                var chart = anychart.graph(data);
+
+                // рисование графа
+                chart.container("network-map").draw();
+
+                // название графа вверху схемы
+                chart.title(false);
+
+                // подписи у точек
+                chart.nodes().labels().enabled(false);
+                chart.nodes().labels().format("{%id}");
+                chart.nodes().labels().fontSize(12);
+                chart.nodes().labels().fontWeight(600);
+
+                // запрет на перемещение точек
+                chart.interactivity().nodes(true);
+
+                // запрет на зум
+                chart.interactivity().zoomOnMouseWheel(true);
+                // запрет на скролл по вертикали
+                chart.interactivity().scrollOnMouseWheel(false);
+
+                // угол наклона графа
+                chart.rotation(30);
+
+                // всплывающие окна у точек
+                chart.nodes().tooltip().useHtml(true);
+                chart.nodes().tooltip().format(
+                    "<span style='font-weight:bold; color:white;'>{%id}: {%status}</span><br><span style='color:white;'>IP-адрес: {%ip}<br>MAC-адрес: {%mac}</span>"
+                );
+
+                // подписи на рёбрах
+                chart.edges().tooltip(false);
+
+                // настройка отображения рёбер
+                chart.edges().normal().stroke("#ffffff", 2);
+                chart.edges().hovered().stroke("#ffa000", 2, "10 5");
+                chart.edges().selected().stroke("#ffa000", 4);
+
+
+                //настройка фона
+                chart.background().fill("#A6B1E1");
+                /*
+                    chart.background().fill({
+                        src: "/static/main/img/bg.svg",
+                        mode: "stretch"
+                    });
+                */
+            })
+        })
+        .catch(e => alert(e))
+}
 function graph() {
+
     //Карта сети
     anychart.data.loadJsonFile('data.json', function (data) {
         // создание графа из загруженных данных
